54df1153006a08e7d07b79c9398b441e0df890d6
Adrian Baddeley <adrian.baddeley@uwa.edu.au>
2017-11-20 14:20:52 +0800

    Minor

--- src/init.c
+++ src/init.c
@@ -12,8 +12,16 @@
 #include <stdlib.h> // for NULL
 #include <R_ext/Rdynload.h>
 
+/*  
+   See proto.h for declarations for the native routines registered below.
+*/
+
 static const R_CMethodDef CEntries[] = {
     {"Cmatchxy",    (DL_FUNC) &Cmatchxy,     7},
+    {"CSmatch2int", (DL_FUNC) &CSmatch2int,  7},
+    {"CSmatch3int", (DL_FUNC) &CSmatch3int,  9},
+    {"CUmatch2int", (DL_FUNC) &CUmatch2int,  7},
+    {"CUmatch3int", (DL_FUNC) &CUmatch3int,  9},
     {"drevcumsum",  (DL_FUNC) &drevcumsum,   2},
     {"fastinterv",  (DL_FUNC) &fastinterv,   5},
     {"inxyp",       (DL_FUNC) &inxyp,        8},
@@ -24,10 +32,6 @@ static const R_CMethodDef CEntries[] = {
     {"ply3sum",     (DL_FUNC) &ply3sum,     10},
     {"prdist2segs", (DL_FUNC) &prdist2segs, 10},
     {"primefax",    (DL_FUNC) &primefax,     3},
-    {"CSmatch2int", (DL_FUNC) &CSmatch2int,  7},
-    {"CUmatch2int", (DL_FUNC) &CUmatch2int,  7},
-    {"CSmatch3int", (DL_FUNC) &CSmatch3int,  9},
-    {"CUmatch3int", (DL_FUNC) &CUmatch3int,  9},
     {NULL, NULL, 0}
 };
 
