bf6b155f6128409b663cf36081cf2265aa9b6bef
Adrian Baddeley <adrian.baddeley@uwa.edu.au>
2019-03-05 16:20:44 +0800

    New function taperoff

---
+++ R/taperoff.R
@@ -0,0 +1,25 @@
+#'
+#'   taperoff.R
+#'
+#'  $Revision: 1.1 $ $Date: 2019/03/05 08:16:36 $
+#'
+
+taperoff <- function(x, zeropoint=0, onepoint=1,
+                     type=c("smooth", "cosine", "Gaussian")) {
+  type <- match.arg(type)
+  #'   cosine taper is standard in engineering (apparently)
+  #' 
+  #'   smooth taper is the pure mathematicians' favorite example
+  #'                of a Smooth Partition of Unity
+  y <- (x-zeropoint)/(onepoint - zeropoint)
+  z <- switch(type,
+              cosine = ifelse(y <= 0, 0,
+                              ifelse(y >= 1, 1,
+                                     (1 - cos(pi * y))/2)),
+              smooth = ifelse(y <= 0, 0,
+                              ifelse(y >= 1, 1,
+                                     exp(-1/y)/(exp(-1/y) + exp(-1/(1-y))))),
+              Gaussian=pnorm(y, mean=1/2, sd=1/6))
+  return(z)
+}
+
